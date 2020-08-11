cbc2c123af4df49f5b4b355156c224c326289726
anton <antoni.wrobel@senstill.pl>
2020-08-11 23:05:14 +0200

    data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,16 @@
 import React from "react"
+import { graphql } from "gatsby"
 
-export default function Home() {
-  return <div>hello</div>
+export default function Home({ data }) {
+  return <pre>{JSON.stringify(data, null, 4)}</pre>
 }
+
+export const query = graphql`
+  {
+    allDatoCmsMenu(filter: { day: { eq: "Poniedziałek" } }) {
+      nodes {
+        breakfast
+      }
+    }
+  }
+`
