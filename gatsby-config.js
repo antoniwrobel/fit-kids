746ecad83f34bba9cdfdd2baa722ed874d938e9b
anton <antoni.wrobel@senstill.pl>
2020-08-11 18:51:14 +0300

    add-files

---
+++ gatsby-config.js
@@ -0,0 +1,43 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
+require("dotenv").config({
+  path: `.env`,
+})
+
+const { default: config } = require("gatsby-plugin-config")
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: config.DATOCMS_READONLY_TOKEN,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: [
+            "Saira Semi Condensed:900,800,700,600,500,400,300,200,100",
+            "Acme",
+          ],
+        },
+      },
+    },
+  ],
+}
